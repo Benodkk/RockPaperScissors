1fa0edf48bb06918d869f48c28a95071127a7a8e
Daniel Kozlowski <daniel.kozlowski@vp.pl>
2022-03-21 23:23:35 +0100

    Zmiana na i dodanie przyciskow

--- JavaScript.js
+++ JavaScript.js
@@ -3,58 +3,59 @@ let computerScore = 0;
 
 let x = Math.random()*3;
 function computerPlay(){
-    if (x<1) {return ('paper');
-}   else if (x<2) {return ('rock');
-}   else {return ('scissors');
+    if (x<1) {return ('Paper');
+}   else if (x<2) {return ('Rock');
+}   else {return ('Scissors');
 }
 }
-computerPlay()
-console.log (x);
-
 
 function playRound(playerSelection, computerSelection){
-    if ((playerSelection=='paper')&&computerSelection=='rock') {
-        playerScore++;
-        return ('Computer chose rock, you won');
-}   else if ((playerSelection=='paper')&&computerSelection=='scissors') {
-        computerScore++;
-        return ('Computer chose scissors, you lose');
-}   else if ((playerSelection=='rock')&&computerSelection=='scissors') {
-        playerScore++;
-        return ('Computer chose scissors, you won');
-}   else if ((playerSelection=='rock')&&computerSelection=='paper') {
-        computerScore++;
-        return ('Computer chose paper, you lose');
-}   else if ((playerSelection=='scissors')&&computerSelection=='paper') {
-        playerScore++;
-        return ('Computer chose paper, you won');
-}   else if ((playerSelection=='scissors')&&computerSelection=='rock') {
-        computerScore++;
-        return ('Computer chose rock,you lose');
-}   else {return ('tie');
-}
-}
-
-
-
-for (let i = 0; i < 5; i++) {
-        let playerSelection = (prompt("Choose:Rock, Paper or Scissors").toLowerCase());
-        let computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection));
-}
-
-console.log ("You got "+playerScore+" points");
-console.log ("Computer got "+computerScore+" points");
-
-function game(){
-        if (playerScore>computerScore) {
-                console.log('You won the match')
+        console.log (`You selected ${playerSelection}`); // Tutaj musialem uzyc `` zeby to zadzialalo
+        console.log (`Computer selected ${computerSelection}`);
+        if ((playerSelection=='Paper') && (computerSelection=='Rock')){
+                console.log ('You won!')   
         }
-        else if (computerScore>playerScore) {
-                console.log('You lose the match')
+        else if ((playerSelection=='Paper') && (computerSelection=='Scissors')){
+                console.log ('You lose.')   
         }
-        else {console.log('Its a tie')}
+        else if((playerSelection=='Scissors') && (computerSelection=='Rock')){
+                console.log ('You lose.')   
+        }
+        else if ((playerSelection=='Scissors')&&(computerSelection=='Paper')){
+                console.log ('You won.')   
+        }
+        else if((playerSelection=='Rock') && (computerSelection=='Paper')){
+                console.log ('You lose.')   
+        }
+        else if ((playerSelection=='Rock') && (computerSelection=='Scissors')){
+                console.log ('You won.')   
+        }
+        else {
+                console.log ('Tie!')
+        }
 }
-game()
-     
 
+
+const Paper= document.querySelector('.Paper')
+const Rock= document.querySelector('.Rock')
+const Scissors= document.querySelector('.Scissors')
+
+function chosePaper(){
+        game('Paper')
+}
+function choseRock(){
+        game('Rock')
+}
+function choseScissors(){
+        game('Scissors')
+}
+
+function game(playerChoice){
+        playRound(playerChoice, computerPlay())
+}
+
+Paper.addEventListener('click',chosePaper)
+
+Rock.addEventListener('click',choseRock)
+
+Scissors.addEventListener('click',choseScissors)
